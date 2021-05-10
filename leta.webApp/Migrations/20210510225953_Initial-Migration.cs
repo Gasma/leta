ee1bd73cef87197720e476f1c7233aba2d768e6a
Gabriel Santos Moreira Alvarenga <gabriel.moreira.alvarenga@gmail.com>
2021-05-10 20:07:25 -0300

    Adiciona migration

---
+++ leta.webApp/Migrations/20210510225953_Initial-Migration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace leta.webApp.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "RouteTime",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DiaDoMes = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DiaDaSemana = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Tempo = table.Column<int>(type: "int", nullable: false),
+                    HoraDoDia = table.Column<TimeSpan>(type: "time(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RouteTime", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RouteTime");
+        }
+    }
+}
